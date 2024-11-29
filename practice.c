59fbe0265cc930c8a311c5634ad06ab6a08a417f
umaryaaniage <umarbuttexe@gmail.com>
2024-11-29 18:18:43 +0500

    c program by 1214

---
+++ practice.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    // Ask the user for a number
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    // If-else statement to check if the number is positive, negative, or zero
+    if (number > 0) {
+        std::cout << "The number is positive.\n";
+    } else if (number < 0) {
+        std::cout << "The number is negative.\n";
+    }
+    
